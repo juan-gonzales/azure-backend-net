35b3f4260e638f9d335e4cf90ac4988ed9f03dc3
Juan Raul Gonzales Luque <54915037+juan-gonzales@users.noreply.github.com>
2025-06-06 16:48:06 -0500

    Add Productos API with Dapper

---
+++ ProductosApi/Program.cs
@@ -0,0 +1,30 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register IDbConnection for Dapper
+builder.Services.AddTransient<IDbConnection>(_ =>
+    new SqlConnection(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
